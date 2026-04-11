4a3eb4e437b2a7315bf38e2f45d2bfc69b0bc922
oljh <oljh@itu.dk>
2026-04-11 15:21:49 +0200

    Add charge to battery and event

--- Assets/Scripts/Modules/Battery.cs
+++ Assets/Scripts/Modules/Battery.cs
@@ -6,19 +6,29 @@ namespace Modules
     [Serializable]
     public class Battery
     {
+        public Action<float, float> OnBatteryLevelChanged;
+        
         [field: SerializeField]
-        private float Charge { get; set; }
+        public float Charge { get; private set; }
 
         public void AddCharge(float charge)
         {
+            float previousCharge = Charge;
+            
             Charge += charge;
             Charge = Mathf.Min(Charge, 1f);
+            
+            OnBatteryLevelChanged?.Invoke(previousCharge, Charge);
         }
         
         public void RemoveCharge(float charge)
         {
+            float previousCharge = Charge;
+            
             Charge -= charge;
             Charge = Mathf.Max(Charge, 0f);
+            
+            OnBatteryLevelChanged?.Invoke(previousCharge, Charge);
         }
     }
 }
