ce10b9b120785c1a25c8b48d316b7f2cc84d7a4f
oljh <oljh@itu.dk>
2026-04-12 13:50:48 +0200

    Cable manager implemented

--- Assets/Scripts/CableManager.cs
+++ Assets/Scripts/CableManager.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class CableManager : MonoBehaviour
 {
-   
+   [SerializeField] private Material _cableDefaultMaterial;
+   [SerializeField] private Material _cableActiveMaterial;
+
+   public void SetCableActive(Renderer cable)
+   {
+      cable.material = _cableActiveMaterial;
+   }
+
+   public void SetCableDefault(Renderer cable)
+   {
+      cable.material = _cableDefaultMaterial;
+   }
 
 }
